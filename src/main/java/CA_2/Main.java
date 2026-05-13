de7aba5702a9665c8d63a028437d69e783c5b742
BrenoQueiroga <84147275+BrenoQueiroga@users.noreply.github.com>
2026-05-13 10:53:04 +0100

    Bank Organisation System started.

---
+++ src/main/java/CA_2/Main.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package CA_2;
+
+/**
+ * Main class for the Bank Organisation System
+ * @author breno
+ */
+public class Main {
+
+    public static void main(String[] args) {
+
+        // Simple message to test if the project is running
+        System.out.println("Bank Organisation System started.");
+    }
+}
---
+++ src/main/java/CA_2/MenuOption.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package CA_2;
+
+/**
+ * Enum used to represent the menu options.
+ * @author breno
+ */
+public enum MenuOption {
+
+    SORT,
+    SEARCH,
+    ADD_RECORD,
+    CREATE_BINARY_TREE,
+    EXIT
+}
